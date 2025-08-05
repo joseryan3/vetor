c539ca0680f7e05f62acb3f8e77c677241405503
José Ryan <joseryan2929@gmail.com>
2025-08-05 16:42:36 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -6,15 +6,15 @@ public class Main {
         System.out.print("Digite um número para ver a tabuada: ");
         int numero = scanner.nextInt();
 
-        // Cria um array para armazenar os resultados da tabuada
+
         int[] tabuada = new int[10];
 
-        // Preenche o vetor com os resultados da tabuada
+ 
         for (int i = 0; i < 10; i++) {
             tabuada[i] = numero * (i + 1);
         }
 
-        // Exibe a tabuada usando o vetor
+   
         for (int i = 0; i < 10; i++) {
             System.out.println(numero + " x " + (i + 1) + " = " + tabuada[i]);
         }
